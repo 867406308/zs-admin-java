c81abcaeb3b9e9654518d05f534383c3b6ae7c65
867406308 <32955236+867406308@users.noreply.github.com>
2023-04-25 22:39:35 +0800

    修改mysql链接工具

--- zs-admin/src/main/java/com/zs/modules/sys/org/domain/entity/SysOrgEntity.java
+++ zs-admin/src/main/java/com/zs/modules/sys/org/domain/entity/SysOrgEntity.java
@@ -1,9 +1,11 @@
 package com.zs.modules.sys.org.domain.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName("sys_org")
 public class SysOrgEntity {
 
     @TableId
--- zs-admin/src/main/java/com/zs/modules/sys/org/mapper/SysOrgMapper.java
+++ zs-admin/src/main/java/com/zs/modules/sys/org/mapper/SysOrgMapper.java
@@ -1,9 +1,9 @@
 package com.zs.modules.sys.org.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import generator.domain.SysOrg;
+import com.zs.modules.sys.org.domain.entity.SysOrgEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface SysOrgMapper extends BaseMapper<SysOrg> {
+public interface SysOrgMapper extends BaseMapper<SysOrgEntity> {
 }
