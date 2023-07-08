d7ecc5e0aa55ac69e7005ea9798e60756d7cf2be
867406308 <32955236+867406308@users.noreply.github.com>
2023-07-08 09:12:27 +0800

    修复mavn出错

--- zs-admin/src/test/java/com/zs/admin/DemoTest.java
+++ zs-admin/src/test/java/com/zs/admin/DemoTest.java
@@ -11,38 +11,4 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class DemoTest {
-
-    @Autowired
-    private SysLogLoginMapper sysLoginLogMapper;
-
-    @Test
-    public void  test(){
-        SysLogLoginAddParams sysLoginLogAddParams = new SysLogLoginAddParams();
-        sysLoginLogAddParams.setUsername("111");
-        sysLoginLogAddParams.setIpAddress("111");
-        sysLoginLogAddParams.setCity("111");
-        sysLoginLogAddParams.setUserAgent("111");
-        sysLoginLogAddParams.setLoginStatus(1);
-        sysLoginLogAddParams.setFailureReason(null);
-        sysLoginLogAddParams.setLoginMethod(1);
-        sysLoginLogAddParams.setLoginSource(null);
-        sysLoginLogAddParams.setBrowser("111");
-        sysLoginLogAddParams.setOs("111");
-
-        SysLogLoginEntity sysLoginLogEntity = BeanUtil.copyProperties(sysLoginLogAddParams, SysLogLoginEntity.class);
-
-        sysLoginLogMapper.insert(sysLoginLogEntity);
-    }
-
-    @Autowired
-    private SysRoleMapper sysRoleMapper;
-    @Test
-    public void test1(){
-        SysRoleEntity sysRoleEntity = new SysRoleEntity();
-        sysRoleEntity.setRoleName("ceshi");
-
-        sysRoleMapper.insert(sysRoleEntity);
-
-
-    }
 }
